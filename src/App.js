060471a11e8c7d2f5fe15262f119eed4c6732e33
YugilWon <yugil3150@naver.com>
2022-03-23 19:36:53 +0900

    Event Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
-  console.log('props', props, props.title);
-  return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
-}
-function Nav(props){
-const lis =[]
-for(let i=0; i < props.topics.length; i++) {
-  let t = props.topics[i];
-  lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
-}
-return <nav>
-  <ol>
-    {lis}
-    </ol>
-  </nav>
-}
+import{Button, Progress} from "semantic-ui-react";
 
 function Article(props){
   console.log('props', props, props.title);
@@ -25,6 +8,33 @@ function Article(props){
     <h2>{props.title}</h2>
     {props.body}
   </article>
+}
+function Header(props){
+  console.log('props', props, props.title);
+  return <header>
+    <h1><a href="/" onClick={(event)=>{
+       event.preventDefault();
+       props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props){
+  const lis =[]
+  for(let i=0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=> {
+           event.preventDefault();
+           props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
 }
 
 function App() {
@@ -35,8 +45,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="OH YEAH"></Header>
-      <Nav topics ={topics}></Nav>
+      <Header title="OH YEAH" onChangeMode={()=>{
+            alert('Header');
+      }}></Header>
+      <Nav topics ={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB!!"></Article>
     </div>
   );
